cb07e466c4908ed82dd2aeb8a65820cc5445b6e1
unknown <Dhanshree-Sarade>
2024-10-07 12:48:27 +0530

    first commit

---
+++ src/main/java/com/ezio/LabourMS/LabourMsApplication.java
@@ -0,0 +1,13 @@
+package com.ezio.LabourMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabourMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabourMsApplication.class, args);
+	}
+
+}
